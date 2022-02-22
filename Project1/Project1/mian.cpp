48dbafcb812a6238470d452e45c7838bf4e74a90
youann12 <50104058+youann12@users.noreply.github.com>
2022-02-22 23:07:04 +0800

    start work on software rendering
    linked libs
    init glfw window

---
+++ Project1/Project1/mian.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main()
+{
+	//初始化glfw
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//创建glfw窗口
+	GLFWwindow* window = glfwCreateWindow(800, 800, "SoftwareRendering", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to initialize GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	//初始化glad
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	//设置视口
+	glViewport(0, 0, 800, 800);
+
+	//改变窗口大小的回调
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
